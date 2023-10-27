c6dc2c88fa10234170ec36f1d76e3453c8692516
balay <dlolhd@gmail.com>
2023-10-27 11:07:59 +0200

    ButtonsFragment implementation.

--- app/src/main/java/com/dlolhd/androidhilt/ui/ButtonsFragment.kt
+++ app/src/main/java/com/dlolhd/androidhilt/ui/ButtonsFragment.kt
@@ -1,16 +1,25 @@
 package com.dlolhd.androidhilt.ui
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import com.dlolhd.androidhilt.AndroidHiltApplication
 import com.dlolhd.androidhilt.R
+import com.dlolhd.androidhilt.data.LoggerLocalDataSource
+import com.dlolhd.androidhilt.navigator.AppNavigator
+import com.dlolhd.androidhilt.navigator.Screens
 
 /**
  * Fragment that displays buttons whose interactions are recorded.
  */
 class ButtonsFragment : Fragment() {
+
+    private lateinit var logger: LoggerLocalDataSource
+    private lateinit var navigator: AppNavigator
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,7 +30,34 @@ class ButtonsFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        view.findViewById<Button>(R.id.button1).setOnClickListener {
+            logger.addLog("Logger 1", "Interaction with 'Button 1")
+        }
+        view.findViewById<Button>(R.id.button2).setOnClickListener {
+            logger.addLog("Logger 2", "Interaction with 'Button 2")
+        }
+        view.findViewById<Button>(R.id.button3).setOnClickListener {
+            logger.addLog("Logger 3", "Interaction with 'Button 3")
+        }
+        view.findViewById<Button>(R.id.all_logs).setOnClickListener {
+            navigator.navigateTo(Screens.LOGS)
+        }
+        view.findViewById<Button>(R.id.delete_logs).setOnClickListener {
+            logger.removeLogs()
+        }
+    }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        populateFields(context)
+    }
+
+    private fun populateFields(context: Context) {
+        logger = (context.applicationContext as AndroidHiltApplication)
+            .appContainer.loggerDataSource
+        navigator = (context.applicationContext as AndroidHiltApplication)
+            .appContainer.provideNavigator(requireActivity())
     }
 
 }
