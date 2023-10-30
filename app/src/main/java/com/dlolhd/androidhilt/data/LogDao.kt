da0951c1debba9c2ec8e5afcfe1ee03223a5b9dd
balay <dlolhd@gmail.com>
2023-10-30 12:22:24 +0100

    @EntryPoint annotation.

--- app/src/main/java/com/dlolhd/androidhilt/data/LogDao.kt
+++ app/src/main/java/com/dlolhd/androidhilt/data/LogDao.kt
@@ -1,5 +1,6 @@
 package com.dlolhd.androidhilt.data
 
+import android.database.Cursor
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
@@ -17,4 +18,10 @@ interface LogDao {
 
     @Query("DELETE FROM logs")
     fun nukeTable()
+
+    @Query("SELECT * FROM logs ORDER BY id DESC")
+    fun selectAllLogsCursor(): Cursor
+
+    @Query("SELECT * FROM logs WHERE id = :id")
+    fun selectLogById(id: Long): Cursor?
 }
